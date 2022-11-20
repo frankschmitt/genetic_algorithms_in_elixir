eca1906aa214fd79e264ac7f7f1d3a55b24ccddc
Frank Schmitt <frankschmitt@gmx.de>
2022-11-20 16:44:45 +0100

    use framework / modules for one_max

--- genetic/genetic/scripts/one_max.exs
+++ genetic/genetic/scripts/one_max.exs
@@ -1,11 +1,22 @@
-chromosome_size = 1000
-max_fitness = chromosome_size # maximum = each bit is 1
+defmodule OneMax do
+  @behaviour Problem
+  alias Types.Chromosome
 
-genotype = fn -> for _ <- 1..chromosome_size, do: Enum.random(0..1) end
+  @impl true
+  def genotype do
+    genes = for _ <- 1..42, do: Enum.random(0..1)
+    %Chromosome{genes: genes, size: 42}
+  end
 
-fitness_function = fn chromosome -> Enum.sum(chromosome) end
+  @impl true
+  def fitness_function(chromosome), do: Enum.sum(chromosome.genes)
 
+  @impl true
+  def terminate?([ best | _]), do: best.fitness == 42
+end
 
-solution = Genetic.run(fitness_function, genotype, max_fitness)
+solution = Genetic.run(OneMax)
+
 IO.write("\n")
 IO.inspect(solution)
+
